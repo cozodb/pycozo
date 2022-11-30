727e3b2edc7e3fb44455026661233b3ca871edbe
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-30 23:03:59 +0800

    update license

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#  Copyright 2022, The Cozo Project Authors.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+#  If a copy of the MPL was not distributed with this file,
+#  You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from setuptools import setup
 
 setup(
@@ -5,7 +11,7 @@ setup(
     version='0.2.1',
     packages=['pycozo'],
     url='',
-    license='MIT',
+    license='MPL-2.0',
     author='Ziyang Hu',
     author_email='hu.ziyang@cantab.net',
     description='Python client for the Cozo database',
